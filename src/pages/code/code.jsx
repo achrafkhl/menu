d3e047f962053d37b790288d9b4f43aadac24db8
achraf <acherf2011@gmail.com>
2025-07-21 02:37:27 +0100

    code qr

--- src/pages/code/code.jsx
+++ src/pages/code/code.jsx
@@ -50,7 +50,7 @@ function Code() {
             <div className={styles["header-center"]}>
                 {restaurantName ? (
                     <div className={styles.inside}>
-                    <img src={restaurantName.image_url} alt={restaurantName} />
+                    <img src={restaurantName.logo_url} alt={restaurantName} />
                     <h3>{restaurantName.name}</h3>
                 </div>
                 ) : (
